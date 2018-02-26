d8a11d72b7fa26e296a3601846911a7a6677d1b7
Angela Ordonez <angelitaooo@me.com>
2018-02-25 20:35:11 -0500

    create cats

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  cats = [
+    {
+      image: "assets/images/felix.jpg",
+      name: "Felix",
+      description: "Felix is 12 years old and is the best cat in the world."
+    },
+    {
+      image: "assets/images/donatella.jpg",
+      name: "Donatella",
+      description: "Donatella is 1 year old and is so funny."
+    },
+    {
+      image: "assets/images/mapale.jpg",
+      name: "Mapale",
+      description: "Mapale is 7 years old is the cutest cat."
+    },
+    {
+      image: "assets/images/milu.jpg",
+      name: "Milu",
+      description: "Milu is 5 years old and is bossy."
+    }
+  ];
 }
