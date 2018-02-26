5dc74f47566d0b03eea838376bc8dc24d7b30c68
Angela Ordonez <angelitaooo@me.com>
2018-02-25 21:06:32 -0500

    style the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  // cats = [
+  //   {
+  //     image: "assets/images/felix.jpg",
+  //     name: "Felix",
+  //     description: "Felix is 12 years old and is the best cat in the world."
+  //   },
+  //   {
+  //     image: "assets/images/donatella.jpg",
+  //     name: "Donatella",
+  //     description: "Donatella is 1 year old and is so funny."
+  //   },
+  //   {
+  //     image: "assets/images/mapale.jpg",
+  //     name: "Mapale",
+  //     description: "Mapale is 7 years old is the cutest cat."
+  //   },
+  //   {
+  //     image: "assets/images/milu.jpg",
+  //     name: "Milu",
+  //     description: "Milu is 5 years old and is bossy."
+  //   }
+  // ];
+
   cats = [
     {
       image: "assets/images/felix.jpg",
       name: "Felix",
-      description: "Felix is 12 years old and is the best cat in the world."
+      description: "Felix tiene 12 años y es el mejor gato del mundo."
     },
     {
       image: "assets/images/donatella.jpg",
       name: "Donatella",
-      description: "Donatella is 1 year old and is so funny."
+      description: "Donatella tiene 1 año y es muy juguetona."
     },
     {
       image: "assets/images/mapale.jpg",
       name: "Mapale",
-      description: "Mapale is 7 years old is the cutest cat."
+      description: "Mapale tiene 7 años y es el mas tierno de todos."
     },
     {
       image: "assets/images/milu.jpg",
       name: "Milu",
-      description: "Milu is 5 years old and is bossy."
+      description: "Milu tiene 5 años y le gusta comer mucho."
     }
   ];
 }
