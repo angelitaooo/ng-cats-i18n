aed302225647ce845188d7b0d129160084d0428b
Angela Ordonez <angelitaooo@me.com>
2018-03-01 20:24:23 -0500

    Setting up the locale of your app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from "@angular/core";
 
 
 import { AppComponent } from './app.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [{ provide: LOCALE_ID, useValue: "en" }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
