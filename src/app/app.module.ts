a89485656bf0845690295f7ecae188bc5224bda0
Angela Ordonez <angelitaooo@me.com>
2018-03-12 09:47:02 -0500

    progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from "@angular/core";
-
-
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [AppComponent],
--- src/main.ts
+++ src/main.ts
@@ -11,18 +11,16 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+// platformBrowserDynamic().bootstrapModule(AppModule);
+// CODE FOR JIT COMPILER
+  // use the require method provided by webpack
+declare const require;
+// we use the webpack raw-loader to return the content as a string
+const translations = require(`raw-loader!./locale/messages.en.xlf`);
 
-platformBrowserDynamic().bootstrapModule(AppModule);
-  // .catch(err => console.log(err));
-
-//   // use the require method provided by webpack
-// declare const require;
-// // we use the webpack raw-loader to return the content as a string
-// const translations = require(`raw-loader!./locale/messages.en.xlf`);
-
-// platformBrowserDynamic().bootstrapModule(AppModule, {
-//   providers: [
-//     { provide: TRANSLATIONS, useValue: translations },
-//     { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' }
-//   ]
-// });
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  providers: [
+    { provide: TRANSLATIONS, useValue: translations },
+    { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' }
+  ]
+});
